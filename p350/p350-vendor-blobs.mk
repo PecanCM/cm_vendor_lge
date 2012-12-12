56f2cd9e3222a63cdce85abb6f518cd431970cb8
RonGokhale <cips173@gmail.com>
2012-12-13 00:59:04 +0530

    p350: remove all common blobs from p350 vendor
    Change-Id: I81cd4cad2e5038c3acac1c90eef05c8aa84e2b28

--- p350/p350-vendor-blobs.mk
+++ p350/p350-vendor-blobs.mk
@@ -58,7 +58,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/p350/proprietary/lib/libcm.so:system/lib/libcm.so \
     vendor/lge/p350/proprietary/lib/libloc.so:system/lib/libloc.so \
     vendor/lge/p350/proprietary/lib/libloc-rpc.so:system/lib/libloc-rpc.so \
-    vendor/lge/p350/proprietary/lib/libmemalloc.so:system/lib/libmemalloc.so \
     vendor/lge/p350/proprietary/lib/libqueue.so:system/lib/libqueue.so \
     vendor/lge/p350/proprietary/lib/libdss.so:system/lib/libdss.so \
     vendor/lge/p350/proprietary/lib/libdll.so:system/lib/libdll.so \
@@ -68,7 +67,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/p350/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
     vendor/lge/p350/proprietary/lib/libsnd.so:system/lib/libsnd.so \
     vendor/lge/p350/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
-    vendor/lge/p350/proprietary/lib/liba2dp.so:system/lib/liba2dp.so \
     vendor/lge/p350/proprietary/lib/libcamera.so:system/lib/libcamera.so \
     vendor/lge/p350/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
     vendor/lge/p350/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
@@ -78,42 +76,10 @@ PRODUCT_COPY_FILES += \
     vendor/lge/p350/proprietary/etc/wl/rtecdc-apsta.bin:system/etc/wl/rtecdc-apsta.bin \
     vendor/lge/p350/proprietary/etc/wl/rtecdc-mfgtest.bin:system/etc/wl/rtecdc-mfgtest.bin \
     vendor/lge/p350/proprietary/etc/MELFAS_FIRM.bin:system/etc/MELFAS_FIRM.bin \
-    vendor/lge/p350/proprietary/etc/vold.fstab:system/etc/vold.fstab \
     vendor/lge/p350/proprietary/bin/ami304d:system/bin/ami304d \
     vendor/lge/p350/proprietary/lib/hw/sensors.pecan.so:system/lib/hw/sensors.pecan.so \
     vendor/lge/p350/proprietary/lib/hw/copybit.default.so:system/lib/hw/copybit.default.so \
     vendor/lge/p350/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so \
-    vendor/lge/p350/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-    vendor/lge/p350/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-    vendor/lge/p350/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-    vendor/lge/p350/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-    vendor/lge/p350/proprietary/bin/dhcpcd:system/bin/dhcpcd \
-    vendor/lge/p350/proprietary/bin/qmuxd:system/bin/qmuxd \
-    vendor/lge/p350/proprietary/bin/wiperiface:system/bin/wiperiface \
     vendor/lge/p350/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
-    vendor/lge/p350/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-    vendor/lge/p350/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
-    vendor/lge/p350/proprietary/lib/libgsl.so:system/lib/libgsl.so \
-    vendor/lge/p350/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-    vendor/lge/p350/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    vendor/lge/p350/proprietary/lib/libOmxAacDec.so:system/lib/libOmxAacDec.so \
-    vendor/lge/p350/proprietary/lib/libOmxAacEnc.so:system/lib/libOmxAacEnc.so \
-    vendor/lge/p350/proprietary/lib/libOmxAdpcmDec.so:system/lib/libOmxAdpcmDec.so \
-    vendor/lge/p350/proprietary/lib/libOmxAmrDec.so:system/lib/libOmxAmrDec.so \
-    vendor/lge/p350/proprietary/lib/libOmxAmrEnc.so:system/lib/libOmxAmrEnc.so \
-    vendor/lge/p350/proprietary/lib/libOmxAmrRtpDec.so:system/lib/libOmxAmrRtpDec.so \
-    vendor/lge/p350/proprietary/lib/libOmxAmrwbDec.so:system/lib/libOmxAmrwbDec.so \
     vendor/lge/p350/proprietary/lib/libOmxEvrcDec.so:system/lib/libOmxEvrcDec.so \
-    vendor/lge/p350/proprietary/lib/libOmxEvrcEnc.so:system/lib/libOmxEvrcEnc.so \
-    vendor/lge/p350/proprietary/lib/libOmxEvrcHwDec.so:system/lib/libOmxEvrcHwDec.so \
-    vendor/lge/p350/proprietary/lib/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
-    vendor/lge/p350/proprietary/lib/libOmxMp3Dec.so:system/lib/libOmxMp3Dec.so \
-    vendor/lge/p350/proprietary/lib/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so \
-    vendor/lge/p350/proprietary/lib/libOmxOn2Dec.so:system/lib/libOmxOn2Dec.so \
-    vendor/lge/p350/proprietary/lib/libOmxQcelp13Enc.so:system/lib/libOmxQcelp13Enc.so \
-    vendor/lge/p350/proprietary/lib/libOmxQcelpHwDec.so:system/lib/libOmxQcelpHwDec.so \
-    vendor/lge/p350/proprietary/lib/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
-    vendor/lge/p350/proprietary/lib/libOmxQcelpDec.so:system/lib/libOmxQcelpDec.so \
-    vendor/lge/p350/proprietary/lib/libOmxWmaDec.so:system/lib/libOmxWmaDec.so \
-    vendor/lge/p350/proprietary/lib/libOmxWmvDec.so:system/lib/libOmxWmvDec.so \
-    vendor/lge/p350/proprietary/lib/libmm-adspsvc.so:system/lib/libmm-adspsvc.so
+    vendor/lge/p350/proprietary/lib/libOmxQcelpDec.so:system/lib/libOmxQcelpDec.so
