28c8e9ba788fa92ac81297a390b276932ecf6534
RonGokhale <cips173@gmail.com>
2013-04-29 10:37:48 +0530

    p350: omx must be under msm7x27-common
    Change-Id: Ibaeefe50eaa052b6eda6a22eea2f7d43a63979ff

--- p350/p350-vendor-blobs.mk
+++ p350/p350-vendor-blobs.mk
@@ -80,6 +80,4 @@ PRODUCT_COPY_FILES += \
     vendor/lge/p350/proprietary/bin/ami304d:system/bin/ami304d \
     vendor/lge/p350/proprietary/lib/hw/sensors.pecan.so:system/lib/hw/sensors.p350.so \
     vendor/lge/p350/proprietary/lib/hw/copybit.default.so:system/lib/hw/copybit.default.so \
-    vendor/lge/p350/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
-    vendor/lge/p350/proprietary/lib/libOmxEvrcDec.so:system/lib/libOmxEvrcDec.so \
-    vendor/lge/p350/proprietary/lib/libOmxQcelpDec.so:system/lib/libOmxQcelpDec.so
+    vendor/lge/p350/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so
